26ee719fbb43ed51593918dd2ae56854d2455485
Sushichan <collin.chew.7188@gmail.com>
2018-06-21 20:32:24 -0700

    1.0

---
+++ 20-jQueryCalculator/Unsolved/calculate.js
@@ -0,0 +1,38 @@
+var number = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+var x = 
+
+
+
+
+function add(x, y) {
+add(x+y)
+}
+
+
+
+
+function subtract(x, y) {
+subtract(x-y)
+}
+
+
+
+
+function multiply(x, y) {
+multiply(x*y)
+}
+
+
+
+
+function divide(x, y) {
+divide(x/y)
+}
+
+
+
+
+function power(x, y) {
+power(x^y)
+}
+
